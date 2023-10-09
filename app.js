3f2ca05d170cffc198f4a370065fed48635664c1
karolbgm <karolyne.morgan@outlook.com>
2023-10-09 16:35:56 -0600

    created game board

---
+++ app.js
@@ -0,0 +1,35 @@
+//Design
+//Create my board/design my wireframe
+
+//Logic
+//Create an array with 4 colors as elements
+//Create an empty array to store computer's selection
+//Make computer choose 4 colors from my colors array randomly and append to my empty array
+//User will select colors from a dropdown color menu for each of the 4 holes
+//Once user completes all holes, they will be able to submit guess 
+//Guess will be saved to an user array
+//Computer will compare user array with its own array
+//Computer will check if:
+//1 - color is contained inside computer array
+//2 - color is in the same index position of computer array
+
+//Conditionals:
+//if color is contained but not on the same position: white peg
+////if color is contained and on the same position: black peg
+//After that, Computer will select a position from feedback array and change color to black or white in the same comparison order
+
+//Checking wins 
+// If feedback grid is all black - user wins, else user guesses again
+//Computer selection is displayed on board
+//Restart option
+
+//Check current guess number
+//do not go over 11
+//If user does not guess correctly within the 11 tries, game is over and computer wins
+//Restar option
+
+
+//Feedback Grid
+//Each circle is going to correspond to a specific index position of a feedback array
+// const guess1 = [position1, position2, positon3, position4]
+
