a6526112f4fb5b36623af1396c566323f72b61cd
karolbgm <karolyne.morgan@outlook.com>
2023-10-16 17:30:58 -0600

    changed color selection and style, pegs style and buttons

--- app.js
+++ app.js
@@ -2,86 +2,49 @@
 
 //Create an array with 4 colors as elements
 
-const colors = ["#ff0000", "#00ff00", "#0000ff", "#ffff00"];
+const colors = [
+  "radial-gradient(circle at 14px 14px, rgb(255, 0, 0), rgb(0, 0, 0))",
+  "radial-gradient(circle at 14px 14px, rgb(0, 0, 255), rgb(0, 0, 0))",
+  "radial-gradient(circle at 14px 14px, rgb(255, 255, 0), rgb(0, 0, 0))",
+  "radial-gradient(circle at 14px 14px, rgb(19, 190, 19), rgb(0, 0, 0))",
+  "radial-gradient(circle at 14px 14px, rgb(255, 165, 0), rgb(0, 0, 0))",
+  "radial-gradient(circle at 14px 14px, rgb(163, 10, 163), rgb(0, 0, 0))",
+];
 
 //Make computer choose 4 colors from my colors array randomly
 
-const color1 = colors[Math.floor(Math.random() * 4)];
-const color2 = colors[Math.floor(Math.random() * 4)];
-const color3 = colors[Math.floor(Math.random() * 4)];
-const color4 = colors[Math.floor(Math.random() * 4)];
+const color1 = colors[Math.floor(Math.random() * 6)];
+const color2 = colors[Math.floor(Math.random() * 6)];
+const color3 = colors[Math.floor(Math.random() * 6)];
+const color4 = colors[Math.floor(Math.random() * 6)];
 const code_maker = [color1, color2, color3, color4];
 
 //User will select colors from a dropdown color menu for each of the 4 holes
 
 //Once user completes all holes, they will be able to submit guess/Guess will be saved to an user array
 
-const colorInput1 = document.querySelector("#firstInput");
-const firstPeg = document.querySelector("#position1.code-breaker");
-
-const colorInput2 = document.querySelector("#secondInput");
-const secondPeg = document.querySelector("#position2.code-breaker");
-
-const colorInput3 = document.querySelector("#thirdInput");
-const thirdPeg = document.querySelector("#position3.code-breaker");
-
-const colorInput4 = document.querySelector("#fourthInput");
-const fourthPeg = document.querySelector("#position4.code-breaker");
-
 const code_breaker = [];
+
 const guessBtn = document.querySelector("#guessBtn");
 
-colorInput1.addEventListener("change", function () {
-  firstPeg.style.backgroundColor = colorInput1.value;
-  if (
-    colorInput1.value === "#ff0000" ||
-    colorInput1.value === "#00ff00" ||
-    colorInput1.value === "#0000ff" ||
-    colorInput1.value === "#ffff00"
-  ) {
-    code_breaker[0] = colorInput1.value;
-    checkEnableGuessBtn();
-  }
-});
+const allColorPegs = document.querySelectorAll(".colors");
+let chosenColor = "";
+for (let i = 0; i < allColorPegs.length; i++) {
+  allColorPegs[i].addEventListener("click", function () {
+    chosenColor = getComputedStyle(allColorPegs[i]).backgroundImage;
+  });
+}
 
-colorInput2.addEventListener("change", function () {
-  secondPeg.style.backgroundColor = colorInput2.value;
-  if (
-    colorInput2.value === "#ff0000" ||
-    colorInput2.value === "#00ff00" ||
-    colorInput2.value === "#0000ff" ||
-    colorInput2.value === "#ffff00"
-  ) {
-    code_breaker[1] = colorInput2.value;
+const guessColors = document.querySelectorAll(".code-breaker");
+for (let i = 0; i < guessColors.length; i++) {
+  guessColors[i].addEventListener("click", function () {
+    guessColors[i].style.background = chosenColor;
+    if (chosenColor !== "") {
+      code_breaker[i] = guessColors[i].style.background;
+    }
     checkEnableGuessBtn();
-  }
-});
-
-colorInput3.addEventListener("change", function () {
-  thirdPeg.style.backgroundColor = colorInput3.value;
-  if (
-    colorInput3.value === "#ff0000" ||
-    colorInput3.value === "#00ff00" ||
-    colorInput3.value === "#0000ff" ||
-    colorInput3.value === "#ffff00"
-  ) {
-    code_breaker[2] = colorInput3.value;
-    checkEnableGuessBtn();
-  }
-});
-
-colorInput4.addEventListener("change", function () {
-  fourthPeg.style.backgroundColor = colorInput4.value;
-  if (
-    colorInput4.value === "#ff0000" ||
-    colorInput4.value === "#00ff00" ||
-    colorInput4.value === "#0000ff" ||
-    colorInput4.value === "#ffff00"
-  ) {
-    code_breaker[3] = colorInput4.value;
-    checkEnableGuessBtn();
-  }
-});
+  });
+}
 
 function checkEnableGuessBtn() {
   if (code_breaker.length === 4) {
@@ -101,10 +64,8 @@ function checkEnableGuessBtn() {
 //if color is contained but not on the same position: white peg
 ////if color is contained and on the same position: black peg
 
-
 function feedback(feedbackList) {
   const feedbackArr = [];
-  // console.log(feedbackArr)
   const allSelectedSpaces = [];
 
   for (let i = 0; i < 4; i++) {
@@ -117,7 +78,6 @@ function feedback(feedbackList) {
 
     feedbackArr.push(randomIndex);
     let selectedSpace = feedbackList[randomIndex];
-    // console.log(selectedSpace)
     allSelectedSpaces.push(selectedSpace);
     if (code_maker[i] === code_breaker[i]) {
       selectedSpace.classList.add("blackPeg");
@@ -127,7 +87,6 @@ function feedback(feedbackList) {
   }
   checkWin(allSelectedSpaces);
 }
-
 
 const feedbackList1 = document.querySelectorAll(".fb1");
 const feedbackList2 = document.querySelectorAll(".fb2");
@@ -139,94 +98,116 @@ const feedbackList7 = document.querySelectorAll(".fb7");
 const feedbackList8 = document.querySelectorAll(".fb8");
 const feedbackList9 = document.querySelectorAll(".fb9");
 const feedbackList10 = document.querySelectorAll(".fb10");
-const feedbackList11 = document.querySelectorAll(".fb11");
 
 //When user clicks Guess, colored pegs will be displayed on board in specific guess number
+
 // let numberOfGuesses = 0;
-let numberOfGuesses = 11;
+
+let numberOfGuesses = 10;
 const everyGuess = document.querySelectorAll(".guess");
 
 guessBtn.addEventListener("click", function () {
   numberOfGuesses--; //starting at 11
-  if (numberOfGuesses === 10) {
+  if (numberOfGuesses === 9) {
     feedbackList = feedbackList1;
-  } else if (numberOfGuesses === 9) {
-    feedbackList = feedbackList2;
   } else if (numberOfGuesses === 8) {
-    feedbackList = feedbackList3;
+    feedbackList = feedbackList2;
   } else if (numberOfGuesses === 7) {
-    feedbackList = feedbackList4;
+    feedbackList = feedbackList3;
   } else if (numberOfGuesses === 6) {
-    feedbackList = feedbackList5;
+    feedbackList = feedbackList4;
   } else if (numberOfGuesses === 5) {
-    feedbackList = feedbackList6;
+    feedbackList = feedbackList5;
   } else if (numberOfGuesses === 4) {
-    feedbackList = feedbackList7;
+    feedbackList = feedbackList6;
   } else if (numberOfGuesses === 3) {
-    feedbackList = feedbackList8;
+    feedbackList = feedbackList7;
   } else if (numberOfGuesses === 2) {
-    feedbackList = feedbackList9;
+    feedbackList = feedbackList8;
   } else if (numberOfGuesses === 1) {
-    feedbackList = feedbackList10;
+    feedbackList = feedbackList9;
   } else if (numberOfGuesses === 0) {
-    feedbackList = feedbackList11;
+    feedbackList = feedbackList10;
   }
   if (numberOfGuesses >= 0) {
-    everyGuess[numberOfGuesses].querySelector(
-      "#position1"
-    ).style.backgroundColor = code_breaker[0];
-    everyGuess[numberOfGuesses].querySelector(
-      "#position2"
-    ).style.backgroundColor = code_breaker[1];
-    everyGuess[numberOfGuesses].querySelector(
-      "#position3"
-    ).style.backgroundColor = code_breaker[2];
-    everyGuess[numberOfGuesses].querySelector(
-      "#position4"
-    ).style.backgroundColor = code_breaker[3];
+    everyGuess[numberOfGuesses].querySelector("#position1").style.background =
+      code_breaker[0];
+    everyGuess[numberOfGuesses].querySelector("#position2").style.background =
+      code_breaker[1];
+    everyGuess[numberOfGuesses].querySelector("#position3").style.background =
+      code_breaker[2];
+    everyGuess[numberOfGuesses].querySelector("#position4").style.background =
+      code_breaker[3];
 
     feedback(feedbackList);
   }
 });
 
-//Checking wins 
+//Checking wins
 // If feedback grid is all black - user wins, else user guesses again
 //If user does not guess correctly within the 11 tries, game is over and computer wins
 //Computer selection is displayed on board
 
-const codemakerPeg1 = document.querySelector('#position1.code-maker')
-const codemakerPeg2 = document.querySelector('#position2.code-maker')
-const codemakerPeg3 = document.querySelector('#position3.code-maker')
-const codemakerPeg4 = document.querySelector('#position4.code-maker')
-const popupMessage = document.querySelector('#pop-message')
-const popupBox = document.querySelector('.popup')
+const codemakerPeg1 = document.querySelector("#position1.code-maker");
+const codemakerPeg2 = document.querySelector("#position2.code-maker");
+const codemakerPeg3 = document.querySelector("#position3.code-maker");
+const codemakerPeg4 = document.querySelector("#position4.code-maker");
+const popupMessage = document.querySelector("#pop-message");
+const popupBox = document.querySelector(".popup");
 function checkWin(allSelectedSpaces) {
-    let correctPegs = 0;
-    for (let element of allSelectedSpaces) {
-      if (element.classList.contains("blackPeg")) {
-        correctPegs++;
-      }
-    }
-    if (numberOfGuesses === 0){
-        if(correctPegs !== 4){
-         displayAnswer()   
-         popupMessage.innerText = 'You lost!'
-         popupBox.style.visibility = 'visible'
-        }
-    }
-    if (correctPegs === 4) {
-      displayAnswer()
-      popupMessage.innerText = 'You won!'  
-      popupBox.style.visibility = 'visible' 
+  let correctPegs = 0;
+  for (let element of allSelectedSpaces) {
+    if (element.classList.contains("blackPeg")) {
+      correctPegs++;
     }
   }
+  if (numberOfGuesses === 0) {
+    if (correctPegs !== 4) {
+      displayAnswer();
+      popupMessage.innerText = "You lost!";
+      popupBox.style.visibility = "visible";
+      guessBtn.disabled = true;
+    }
+  }
+  if (correctPegs === 4) {
+    displayAnswer();
+    popupMessage.innerText = "You won!";
+    popupBox.style.visibility = "visible";
+    guessBtn.disabled = true;
+  }
+}
 
 //Restart option
 
-function displayAnswer (){
-codemakerPeg1.style.backgroundColor = code_maker[0]
-codemakerPeg2.style.backgroundColor = code_maker[1]
-codemakerPeg3.style.backgroundColor = code_maker[2]
-codemakerPeg4.style.backgroundColor = code_maker[3]
+function displayAnswer() {
+  codemakerPeg1.style.background = code_maker[0];
+  codemakerPeg2.style.background = code_maker[1];
+  codemakerPeg3.style.background = code_maker[2];
+  codemakerPeg4.style.background = code_maker[3];
 }
 
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("instructions");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
